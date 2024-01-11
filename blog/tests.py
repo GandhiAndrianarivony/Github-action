60aab2188692ef85920e8823ca6a2638c3752dac
GandhiAndrianarivony <gandhi.andrianarivony@gmail.com>
2024-01-11 13:40:04 +0300

    Create testing model

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from blog.models import Post
+
 
 # Create your tests here.
+class BlogModelTest(TestCase):
+    """Test Blog Model"""
+
+    def setUp(self):
+        self.blog = Post.objects.create(
+            title="django", author="John", slug="django"
+        )
+
+    def test_post_model(self):
+        self.assertTrue(isinstance(self.blog, Post))
+        self.assertEqual(str(self.blog), "django")
